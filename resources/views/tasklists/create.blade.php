4038d2d0e1ea491428e0717c82bdc7ac1b1bb8fe
misato.otaki <misato.otaki@rakuten.com>
2018-06-01 06:46:54 +0000

    bootstrap design

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -4,16 +4,37 @@
 
     <h1>タスクリスト新規作成ページ</h1>
     
-    {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6" >
+            {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
+                
+                <div class="form-group">
+                    {!! Form::label('status','状況:') !!}
+                    {!! Form::text('status', null, ['class' =>'form-control']) !!}
+                    
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('content','タスクリスト:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    
+                    
+                </div>
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
-        {!! Form::label('status','状況:') !!}
-        {!! Form::text('status') !!}
+            {!! Form::close() !!}
+        </div>
         
-        {!! Form::label('content','タスクリスト:') !!}
-        {!! Form::text('content') !!}
         
-        {!! Form::submit('投稿') !!}
         
-    {!! Form::close() !!}
+    </div>
+    
+    
+        
+        
+        
+       
+        
+        
 
 @endsection
--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -4,12 +4,31 @@
 
     <h1>id= {{$tasklist->id }}のタスクリスト詳細ページ</h1>
     
-    <p>状況: {{ $tasklist->status }}</p>
-    <p>タスクリスト{{ $tasklist->content }}</p>
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $tasklist->id }}</td>
+        </tr>
+        <tr>
+            <th>状況</th>
+            <td>{{ $tasklist->status }}</td>
+        </tr>
+        <tr>
+            <th>タスクリスト</th>
+            <td>{{ $tasklist->content }}</td>
+        </tr>
+        
+        
+        
+        
+        
+        
+    </table>
     
-    {!! link_to_route('tasklists.edit','このタスクリストを編集', ['id' =>$tasklist->id]) !!}
+    
+    {!! link_to_route('tasklists.edit','このタスクリストを編集', ['id' =>$tasklist->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id],'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 @endsection
